511e3940da3de0975a1f4b08da918029c54a723f
AndreaGuiman <77883538+andreaguiman@users.noreply.github.com>
2021-01-24 11:54:46 +0200

    5th commit, a bit of fxml and controllers

--- src/controllers/LogInController.java
+++ src/controllers/LogInController.java
@@ -4,14 +4,61 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.Utilizatori;
 
 import java.io.IOException;
+import java.util.List;
 
 public class LogInController {
     @FXML
     Hyperlink signUp;
+    @FXML
+    Button buttonLogIn;
+    @FXML
+    CheckBox verify;
+    @FXML
+    TextField usernameField;
+    @FXML
+    PasswordField passwordField;
+
+    String username, password;
+
+    public void initialize(){
+        buttonLogIn.setDisable(true);
+    }
+
+    public void keyReleasedProperty(){
+        username = usernameField.getText();
+        password = passwordField.getText();
+        buttonLogIn.setDisable((isNullOrEmpty(username, password) || !verify.isSelected()));
+    }
+
+    //verificam daca fiecare casuta de text este completata, daca este completata returnam adevarat, adica activam butonul de log in
+    public static boolean isNullOrEmpty(String... strArr) {
+        for (String st : strArr) {
+            if  (st==null || st.equals(""))
+                return true;
+
+        }
+        return false;
+    }
+
+    public void logInAction() throws Exception{
+        buttonLogIn.getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("../scenes/Menu.fxml"));
+        Stage menuStage = new Stage();
+        Scene menuScene = new Scene(root);
+        //logInScene.getStylesheets().add(getClass().getResource("/resources/css/LogInStylesheet.css").toExternalForm());
+        menuStage.setTitle("MENU");
+        menuStage.setScene(menuScene);
+        menuStage.show();
+    }
+
+    public List<Utilizatori> getAllUsers(){
+        return null;
+    }
 
     public void goToSignUp() throws IOException {
         signUp.getScene().getWindow().hide();
--- src/controllers/SignUpController.java
+++ src/controllers/SignUpController.java
@@ -4,8 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,10 +12,41 @@ import java.io.IOException;
 public class SignUpController {
     @FXML
     Hyperlink logIn;
-
     //dupa ce toate campurile sunt completate, butonul de sign up devine "disable = false", adica il putem folosi
     @FXML
     Button signUp;
+    @FXML
+    TextField usernameField, passwordField, nameField, emailField, telNoField;
+    @FXML
+    PasswordField confirmPasswordField;
+    @FXML
+    CheckBox verify;
+
+    String username, password, confirmPassword, name, email, telNo;
+
+    public void initialize(){
+        signUp.setDisable(true);
+    }
+
+    public void keyReleasedProperty(){
+        username = usernameField.getText();
+        password = passwordField.getText();
+        confirmPassword = confirmPasswordField.getText();
+        name = nameField.getText();
+        email = emailField.getText();
+        telNo = telNoField.getText();
+        signUp.setDisable((isNullOrEmpty(username, password, confirmPassword, name, email, telNo) || !verify.isSelected()));;
+    }
+
+    //verificam daca fiecare casuta de text este completata, daca este completata returnam adevarat, adica activam butonul de log in
+    public static boolean isNullOrEmpty(String... strArr) {
+        for (String st : strArr) {
+            if  (st==null || st.equals(""))
+                return true;
+
+        }
+        return false;
+    }
 
     //pentru hyperlink, ne duce inapoi la login
     public void goToLogIn() throws IOException {
