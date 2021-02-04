7d04885dfe458969fb4d8e4631230e2dc39771e4
AndreaGuiman <77883538+andreaguiman@users.noreply.github.com>
2021-02-04 17:32:25 +0200

    nothing

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("LOGIN");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        ////
     }
 
 
